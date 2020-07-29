6dbb7aa66ec2cc4d3d70b2f8df74e7b2dc725309
Harbour Harrison <harb3gaucho@gmail.com>
2020-07-29 12:15:58 -0700

    added word array to store characters the user wants

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -6,18 +6,18 @@ function writePassword() {
   var passwordLength = prompt("How long do you want your password? (Choose a number between 8 and 128)");
   passwordLength = parseInt(passwordLength);
 
-  var userLowecase = confirm("Do you want lowecase Characters?");
+  var userLowercase = confirm("Do you want lowecase Characters?");
   var userUppercase = confirm("Do you want uppercase Characters?");
   var userNumeric = confirm("Do you want numeric Characters?");
   var userSpecial = confirm("Do you want special Characters?");
 
-  const lowecaseCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  const lowercaseCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   const uppercaseCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   const numericCharacters = ['0','1','2','3','4','5','6','7','8','9'];
   const specialCharacters = ['!','@','#','$','%','^','&','*'];
 
   var charCount = 0;
-  if (userLowecase === true){
+  if (userLowercase === true){
     charCount++
   }
   if (userUppercase === true){
@@ -34,6 +34,27 @@ function writePassword() {
 
   function generatePassword() {
     var word = [];
+
+    if (userLowercase=== true){
+      for (var i=0;i<passwordLength/charCount;i++){
+        word.push(lowercaseCharacters[Math.floor(Math.random() * lowercaseCharacters.length)]);
+      }
+    };
+    if (userUppercase=== true){
+      for (var i=0;i<passwordLength/charCount;i++){
+        word.push(uppercaseCharacters[Math.floor(Math.random() * uppercaseCharacters.length)]);
+      }
+    };
+    if (userNumeric=== true){
+      for (var i=0;i<passwordLength/charCount;i++){
+        word.push(numericCharacters[Math.floor(Math.random() * numericCharacters.length)]);
+      }
+    };
+    if (userSpecial=== true){
+      for (var i=0;i<passwordLength/charCount;i++){
+        word.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
+      }
+    };
 
     return(word);
   }
